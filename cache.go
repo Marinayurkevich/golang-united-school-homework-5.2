0cb33f9ef666832d058eed951c44f887ad369b59
marina <roxy-lo@inbox.ru>
2022-06-27 15:27:58 +0300

    1st try

--- cache.go
+++ cache.go
@@ -9,15 +9,22 @@ func NewCache() Cache {
 	return Cache{}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (f Cache) Get(key string) (string, bool) {
+if ok {
+return Cache.value, true
+}
+else return " ", false
 }
 
-func (receiver) Put(key, value string) {
+func (f Cache) Put(key, value string) {
 }
 
-func (receiver) Keys() []string {
+func (f Cache) Keys() []string {
+keys := make ([]string, 0)
+for key, value := range NewCache {
+keys = append(keys, key)}
+return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (f Cache) PutTill(key, value string, deadline time.Time) {
 }
