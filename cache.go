93808cdeda58db3d014db6518076297e39237109
marina <roxy-lo@inbox.ru>
2022-06-27 16:23:42 +0300

    2

--- cache.go
+++ cache.go
@@ -17,11 +17,10 @@ func NewCache() Cache {
 }
 
 func (f Cache) Get(key string) (string, bool) {
-key, ok:= f.structure[key]
-if !ok {
+MyCache, ok:= f.structure[key]
+if ok {
+return MyCache.value, true}
 return " ", false
-}
-return Cache.value, true
 }
 
 func (f Cache) Put(key, value string) {
@@ -29,7 +28,7 @@ func (f Cache) Put(key, value string) {
 
 func (f Cache) Keys() []string {
 keys := make ([]string, 0)
-for key, value := range NewCache {
+for key, value := range f.structure {
 keys = append(keys, key)}
 return keys
 }
