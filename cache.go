b0d42d09f11c7ff2102600af1634aefbd0502c93
marina <roxy-lo@inbox.ru>
2022-06-29 14:37:03 +0300

    1

--- cache.go
+++ cache.go
@@ -2,14 +2,14 @@ package cache
 
 import "time"
 
-type MyCache struct{
-key string
-value string
-deadline time.Time
+type MyCache struct {
+	key      string
+	value    string
+	deadline time.Time
 }
 
 type Cache struct {
-structure map[string]MyCache
+	structure map[string]MyCache
 }
 
 func NewCache() Cache {
@@ -17,10 +17,11 @@ func NewCache() Cache {
 }
 
 func (f Cache) Get(key string) (string, bool) {
-MyCache, ok:= f.structure[key]
-if ok {
-return MyCache.value, true}
-return " ", false
+	MyCache, ok := f.structure[key]
+	if ok {
+		return MyCache.value, true
+	}
+	return " ", false
 }
 
 func (f Cache) Put(key, value string) {
@@ -28,14 +29,17 @@ func (f Cache) Put(key, value string) {
 }
 
 func (f Cache) Keys() []string {
-keys := make ([]string, 0)
-for key, _ := range f.structure {
-if key >=deadline{
-delete(f.structure,key)
-} else {
-keys = append(keys, key)}}
-return keys
+	keys := make([]string, 0)
+	for key, _ := range f.structure {
+		if key >= deadline {
+			delete(f.structure, key)
+		} else {
+			keys = append(keys, key)
+		}
+	}
+	return keys
 }
 
 func (f Cache) PutTill(key, value string, deadline time.Time) {
+
 }
